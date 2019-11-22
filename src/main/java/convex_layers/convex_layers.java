2ca2f91a7c313738f0f5afc104d6e8d64aafe3fc
Kaj0Wortel <kaj.wortel@gmail.com>
2019-11-22 14:17:48 +0100

    Revert "Renamed main class + removed workspace.xml"
    This reverts commit d2357979b5f7aca43c11ba008b67c455f9887a3f.

--- src/main/java/convex_layers/convex_layers.java
+++ src/main/java/convex_layers/convex_layers.java
@@ -3,7 +3,7 @@ package convex_layers;
 /**
  * Main project class.
  */
-public class ConvexLayers {
+public class convex_layers {
     
     public static void main(String[] args) {
         System.out.println("test");
