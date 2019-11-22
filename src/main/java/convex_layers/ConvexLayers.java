0c44cd1fda6f5ba17f96ff6a6caa743c43b81519
VortexAI <s.c.a.beukers@student.tue.nl>
2019-11-22 14:48:25 +0100

    Created Basic architecture of the software

--- src/main/java/convex_layers/ConvexLayers.java
+++ src/main/java/convex_layers/ConvexLayers.java
@@ -1,10 +1,22 @@
 package convex_layers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Main project class.
  */
 public class ConvexLayers {
-    
+    //Set that keeps track of all variables that still need to be checked.
+    Set<Vertex> todo = new HashSet<Vertex>();
+    //Set containing the edges of the resulting convex partitioning
+    Set<Edge> sol = new HashSet<Edge>();
+    //Linked list of the inner convex hull
+    DoublyLinkedList<Vertex> in;
+    //Linked list of the outer convex hull
+    DoublyLinkedList<Vertex> out;
+
+
     public static void main(String[] args) {
         System.out.println("test");
     }
