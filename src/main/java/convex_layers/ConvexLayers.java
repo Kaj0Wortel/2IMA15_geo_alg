d2357979b5f7aca43c11ba008b67c455f9887a3f
Kaj0Wortel <kaj.wortel@gmail.com>
2019-11-22 14:11:02 +0100

    Renamed main class + removed workspace.xml

--- src/main/java/convex_layers/ConvexLayers.java
+++ src/main/java/convex_layers/ConvexLayers.java
@@ -3,7 +3,7 @@ package convex_layers;
 /**
  * Main project class.
  */
-public class convex_layers {
+public class ConvexLayers {
     
     public static void main(String[] args) {
         System.out.println("test");
